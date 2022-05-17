dafd2c914de3f209fe028caf22598c9ea652ec81
Purcela <purceladzeg@gmail.com>
2022-05-17 17:28:56 +0400

    about

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
   })
   
+function getVideoFr() {
+    
+  var a = document.getElementById("fd_s").value;
+  $.get('config/getVideoJs.php', {a:a}, function(data) {
+    $('#random_code_subitlte').html(data);
+  });
+}
+
+
