036f0eecc0ce733cbe931abddb08a0419d6f76c0
Purcela <purceladzeg@gmail.com>
2022-05-20 17:44:54 +0400

    123

--- js/script.js
+++ js/script.js
@@ -18,17 +18,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-
  
 function myFunction() {
   var x = document.getElementById("myVideo").autoplay;
   document.getElementById("demo").innerHTML = x;
 }
 
-function getVideoFr() {
-	
-  var a = document.getElementById("fd_s").value;
-  $.get('config/getVideoJs.php', {a:a}, function(data) {
-    $('#random_code_subitlte').html(data);
-  });
-}
+
+// ----------------------------------------------------------------------------------------------
+
+// var fallbackToStore = function() {
+//   window.location.replace('https://play.google.com/store/apps/details?id=com.facebook.orca');
+// };
+// var openApp = function() {
+//   window.location.replace('https://purcela.github.io/smartpublishing/index.html');
+// };
+// var triggerAppOpen = function() {
+//   openApp();
+//   setTimeout(fallbackToStore, 250);
+// };
