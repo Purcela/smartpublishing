e0c4bd49f5ecc5ddc85530945306dbf33e755e49
Purcela <purceladzeg@gmail.com>
2022-05-25 20:48:39 +0400

    small changes

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every x seconds
+  setTimeout(showSlides, 4000); // Change image every x seconds
 }
 
 //   ----------------------------------------------- facebook -----------------------------------------------------------
@@ -61,3 +61,18 @@ js = d.createElement(s); js.id = id;
 js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+//   ----------------------------------------------- search ideo -----------------------------------------------------------
+
+
+document.getElementById("newsearch").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  var searchText = document.getElementById("searchvalue").value;
+  var extension = document.getElementById("hiddenvalue1").value;
+
+  // window.location = this.action + searchText + "." + extension;
+  // or if you don't want to use the form's action attribute to specify
+  // the domain you could hardcode it in the function:
+  window.location = "http://127.0.0.1:5500/" + searchText + "." + extension;
+});
